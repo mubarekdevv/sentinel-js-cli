90536740a94a0fd44b1b4d307007611dac3f7561
Mubarek Jemal <mubarekj71@gmail.com>
2026-04-16 09:45:26 +0300

    object

--- data.js
+++ data.js
@@ -4,5 +4,11 @@ export const assets = [
         ticker: "BTC",
         price: 65_000,
         purchasePrice: 60_000,
+    },
+    {
+        name: "Ethereum",
+        ticker: "ETH",
+        price: 3_200,
+        purchasePrice: 3_500
     }
 ]
