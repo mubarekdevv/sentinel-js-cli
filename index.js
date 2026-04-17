794db4d2c9c5797cc186dce28fbb8f2b66317cef
Mubarek Jemal <mubarekj71@gmail.com>
2026-04-17 17:51:25 +0300

    our imported object destructured

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ function displayPortfolio(myArray) {
   myArray.forEach((asset) => {
     //Date() constsructor for current time stamp
     time = new Date().toLocaleTimeString();
-    
+    // const name = asset.name
+    // const ticker = asset.ticker
+    // const price = asset.price
+    // const purchasePrice = asset.purchasePrice
+    // But b/c asset is an object so I can destructure it but assets is a list
+    const { name, ticker, price, purchasePrice } = asset
     
     
   });
 }
 
 // displayPortfolio(assets)
+
+
