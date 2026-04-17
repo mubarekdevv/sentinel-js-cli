504b5b0bbf478c9896367bd4484ec6fab9425b90
Mubarek Jemal <mubarekj71@gmail.com>
2026-04-17 17:42:07 +0300

    current time stamp

---
+++ index.js
@@ -0,0 +1,15 @@
+import { assets } from "./data.js";
+
+console.log("--- Sentinel Portfolio Tracker Initialized ---");
+
+function displayPortfolio(myArray) {
+  myArray.forEach((asset) => {
+    //Date() constsructor for current time stamp
+    time = new Date().toLocaleTimeString();
+    
+    
+    
+  });
+}
+
+// displayPortfolio(assets)
