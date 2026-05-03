0fa3e814f1623c993cf75e1ffdea0e0efa14de1a
Mubarek Jemal <mubarekj71@gmail.com>
2026-05-03 21:56:11 +0300

    ll

--- index.js
+++ index.js
@@ -270,3 +270,11 @@ function logAnswer(answer, points) {
     `The answer is ${answer} of course! If you got that right, giver yourself ${points} points.`,
   );
 }
+
+console.log('What is the capital of Peru?')
+/*
+Challenge:
+    1. After a 3 second delay, call the 'logAnswer' function.
+    2. Make sure 'logAnswer' has all the info it needs. 
+       The answer is 'Lima' and it's 10 points for getting it right. 
+*/
